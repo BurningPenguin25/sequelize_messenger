0c747dd859282f32d6b4cf49b3f7de9faeadb3c5
BurningPenguin25 <74684220+BurningPenguin25@users.noreply.github.com>
2022-11-25 13:13:16 +0300

    sequelize_messenger
    Создание проекта
    Подключение к БД
    Миграция
    Инициализация

---
+++ sequelize_connection.js
@@ -0,0 +1,17 @@
+const {Sequelize} = require('sequelize');
+
+const sequelize = new Sequelize('dbase_messenger', 'root', 'BurningPenguin25b', {
+    host: 'localhost',
+    dialect: 'mysql' ,
+});
+
+async function start() {
+    try {
+        await sequelize.authenticate();
+        console.log('Connection has been established successfully.');
+    } catch (error) {
+        console.error('Unable to connect to the database:', error);
+    }
+}
+start()
+
