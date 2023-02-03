6b282691f96df1ffdb6c3eba0b78b810c0a63f91
BurningPenguin25 <74684220+BurningPenguin25@users.noreply.github.com>
2023-02-03 23:45:09 +0300

    Sequelize queries 1) подключение 2) настройка 3) работа с таблицей: findByPk, destroy, findAll, update_

--- user_class.js
+++ user_class.js
@@ -1,81 +1,51 @@
 const {User} = require('./models/index.js')
 
 class dataUser{
+    _id
+    _UserName;
+    _UserSurname;
+    // _UserPatronymic;
+    // _UserTelNumber;
+    // _UserLogin;
+    // _UserEMail;
+    // _UserLicense;
+    // _createdAt;
+    // _updatedAt;
 
-//  userID(i){
-//     return User.findByPk(i)
-// }
-//
-// userCreate(dt){ // отправляет в базу V
-//     return User.create(dt)
-// }
-//
-//  DeleteString(del){
-//     return User.destroy(del)
-// }
-
-//  FindAllStrings(fa){
-//     return User.findAll(fa)
-// }
-
-    UpdateString(upd){
-        return User.update(upd).then((data)=>{
-            console.log(data)
-        }).catch((err)=>{
-            console.log(err)
-        })
+    getId(i) {
+        return User.findByPk(i)
     }
 
+   set UserID(id){
+        this._id = id
+       console.log('id',id)
+    };
+
+    set UserName(name){
+        this._UserName = name
+        console.log('name',name)
+    };
+
+    set UserSurname(surname){
+        this._UserSurname = surname
+        console.log('surname', surname)
+    }
+
+};
+
+let data = new dataUser();
+
+async function get(){
+    const a = await data.getId(1)
+
+    data.UserID = a.dataValues.id
+    data.UserName = a.dataValues.UserName
+    data.UserSurname = a.dataValues.UserSurname
+    //console.log(a)
 }
 
-const dataus = new dataUser();
-
-( async ()=> {
-    // const get = await dataus.userID(6)
-    // console.log(get)
-
-    // const post = await dataus.userCreate(
-    //     {
-    //         UserName: 'John3',
-    //         UserSurname: 'Doe3',
-    //         UserPatronymic: 'example@example.com3',
-    //         UserTelNumber: '12345678903',
-    //         UserLogin: 'loginUser3',
-    //         UserEMail: 'loginEmail@mail.com3',
-    //         UserLicense: 'commercial3',
-    //         createdAt: new Date(),
-    //         updatedAt: new Date()
-    //     },
-    // );
-
-    // const des = await dataus.DeleteString(1)
-
-//     const findAll = await dataus.FindAllStrings({
-// where:{
-//     UserName: 'Paul',
-//     UserEMail: 'John3'
-// }
-//     })
-
-   const updg = await dataus.UpdateString(
-       { UserName:"Paul5"},
-       {
-           where: {
-               id: 2
-           }
-       }
-   )
-
-})();
+get()
 
 
 
-// Создать - Аналог операции insert into Users values / .create V
-//
-//     Удалить - Аналог операции delete from Users where … / .destroy V
-//
-//     Выбрать по любому из обязательных или необязательных полей - Аналог операции select * from Users where … / .findAll V
-//
-//     Обновить данные текущего пользователя - Аналог операции update Users set … where … / .update Х
-//
-//     Обновить данные пользователя, выбирая по любому из полей
+
