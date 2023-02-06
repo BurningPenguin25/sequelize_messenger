e970e54b30dd9657a717766ad697c9d21b87c583
BurningPenguin25 <74684220+BurningPenguin25@users.noreply.github.com>
2023-02-06 20:41:08 +0300

    Поля класса - undef

--- user_class.js
+++ user_class.js
@@ -1,51 +1,117 @@
 const {User} = require('./models/index.js')
 
-class dataUser{
-    _id
-    _UserName;
-    _UserSurname;
-    // _UserPatronymic;
-    // _UserTelNumber;
-    // _UserLogin;
-    // _UserEMail;
-    // _UserLicense;
-    // _createdAt;
-    // _updatedAt;
+// Имена класса всегда начинаются с большой буквы
+// Почему поля приватные
+// Все поля начинаются с маленькой, так же как и методы
+// Оставление консольный логов в коде, отправленном на ревью, является грубейшей небрежностью
+// Имя UserSurname избыточно. Это поле и так находится в классе User. Потом как работать с ним? User.UserSurname? Имя не годится
 
-    getId(i) {
+
+
+class dataUser{
+       id;  // не передаются данные в id. если _id, то все выводится ок
+    // name;
+    // surname;
+    // patronymic;
+    // telnumber;
+    // login;
+    // email;
+    // license;
+
+
+    async getId(i) {
         return User.findByPk(i)
     }
 
-   set UserID(id){
-        this._id = id
-       console.log('id',id)
-    };
-
-    set UserName(name){
-        this._UserName = name
-        console.log('name',name)
-    };
-
-    set UserSurname(surname){
-        this._UserSurname = surname
-        console.log('surname', surname)
+    set id(idd){
+    this.id = idd
+        console.log(idd)
     }
+
+    // set name(nm){
+    //     this.name = nm
+    //
+    // };
+    //
+    // set surname(sn){
+    //     this.surname = sn
+    // };
+    //
+    // set patronymic(pn){
+    //     this.patronymic = pn
+    // };
+    //
+    // set telnumber(tn){
+    //     this.telnumber = tn
+    // };
+    //
+    // set login(lg){
+    //     this.login = lg
+    // };
+    //
+    // set email(ml){
+    //     this.email = ml
+    // };
+    //
+    // set license(lc){
+    //     this.license = lc
+    // };
+    //
+    //
+    // cr(){
+    //   User.create({
+    //       UserName: "Jane",
+    //       UserSurname: "Doe",
+    //       UserPatronymic: "--",
+    //       UserTelNumber: "1234567891010",
+    //       UserLogin:"LOGINLog",
+    //       UserEMail:"Mylo@mylo.com",
+    //       UserLicense:"free",
+    //   }
+    //
+    //   )
+    // }
+
 
 };
 
-let data = new dataUser();
+let datauser = new dataUser();
+
 
 async function get(){
-    const a = await data.getId(1)
+    const a = await datauser.getId(1)
 
-    data.UserID = a.dataValues.id
-    data.UserName = a.dataValues.UserName
-    data.UserSurname = a.dataValues.UserSurname
-    //console.log(a)
+    datauser.id = a.dataValues.id;
+    // datauser.name = a.dataValues.UserName;
+    // datauser.surname = a.dataValues.UserSurname;
+    // datauser.patronymic = a.dataValues.UserPatronymic;
+    // datauser.telnumber = a.dataValues.UserTelNumber;
+    // datauser.login = a.dataValues.UserLogin;
+    // datauser.email = a.dataValues.UserEMail;
+    // datauser.license = a.dataValues.UserLicense;
+
+    // data.cr()
 }
+
 
 get()
 
 
+// Создать - Аналог операции insert into Users values …
+// Удалить - Аналог операции delete from Users where …
+// Выбрать по любому из обязательных или необязательных полей - Аналог операции select * from Users where …
+// Обновить данные текущего пользователя - Аналог операции update Users set … where …
+// Обновить данные пользователя, выбирая по любому из полей
 
-
+// dataValues: {
+//     id: 1,
+//         UserName: 'John',
+//         UserSurname: 'Doe',
+//         UserPatronymic: 'example@example.com',
+//         UserTelNumber: '1234567890',
+//         UserLogin: 'loginUser',
+//         UserEMail: 'loginEmail@mail.com',
+//         UserLicense: 'commercial',
+//         createdAt: 2023-01-30T15:43:28.000Z,
+//         updatedAt: 2023-01-30T15:43:28.000Z
+// }
