0b267e10afcee2ee38e51dcd1d44af55cc279e75
BurningPenguin25 <74684220+BurningPenguin25@users.noreply.github.com>
2023-03-10 01:15:18 +0300

    Тест подключение
    Проверка подключения к БД

--- connect_db.js
+++ connect_db.js
@@ -1,13 +1,26 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('sequelize_messenger', 'root', 'BurningPenguin25b', {
-    host: 'localhost',
-    dialect: 'mysql',
-});
+async function coonect(){
+    const sequelize = new Sequelize('sequelize_messenger', 'roots', 'BurningPenguin25b', { // root
+        host: 'localhost',
+        dialect: 'mysql',
+    });
 
-try {
-    sequelize.authenticate()
-    console.log('Connection has been established successfully.');
-} catch (err) {
-    console.error('Unable to connect to the database:', err)
+    try {
+        await sequelize.authenticate()
+        console.log('Connection has been established successfully.');
+    } catch (err) {
+        console.error('Unable to connect to the database:', err)
+    }
 }
+
+coonect()
+
+
+
+
+
+
+
+
+
